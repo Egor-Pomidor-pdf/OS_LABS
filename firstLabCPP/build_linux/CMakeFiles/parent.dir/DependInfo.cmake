637050fd4ccd760d3c84fead299e68bef7125fb8
Егор Туймуков <egortujmukov4683@gmail.com>
2025-10-13 13:48:35 +0600

    fix report firstLab

---
+++ firstLabCPP/build_linux/CMakeFiles/parent.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/src/child_process.cpp" "CMakeFiles/parent.dir/src/child_process.cpp.o" "gcc" "CMakeFiles/parent.dir/src/child_process.cpp.o.d"
+  "/workspace/src/os_linux.cpp" "CMakeFiles/parent.dir/src/os_linux.cpp.o" "gcc" "CMakeFiles/parent.dir/src/os_linux.cpp.o.d"
+  "/workspace/src/parent.cpp" "CMakeFiles/parent.dir/src/parent.cpp.o" "gcc" "CMakeFiles/parent.dir/src/parent.cpp.o.d"
+  "/workspace/src/parent_process.cpp" "CMakeFiles/parent.dir/src/parent_process.cpp.o" "gcc" "CMakeFiles/parent.dir/src/parent_process.cpp.o.d"
+  "/workspace/src/pipe.cpp" "CMakeFiles/parent.dir/src/pipe.cpp.o" "gcc" "CMakeFiles/parent.dir/src/pipe.cpp.o.d"
+  "/workspace/src/utils.cpp" "CMakeFiles/parent.dir/src/utils.cpp.o" "gcc" "CMakeFiles/parent.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
